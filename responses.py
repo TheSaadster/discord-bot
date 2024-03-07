8e0b8d4abf55d2801a6843c5bf45c4480a100e19
Saad <saadaulakh04@gmail.com>
2024-03-07 15:19:35 -0500

    Added AI Responses

--- responses.py
+++ responses.py
@@ -1,18 +1,18 @@
+import os
 from random import choice, randint
+from typing import Final
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+OPENAI_TOKEN: Final[str] = os.getenv("OPENAI_API_KEY")
+
+
+client = OpenAI()
+
 
 def get_response(user_input: str) -> str:
-    lowered: str = user_input.lower()
-    if lowered == '':
-        return 'Well you\'re awfully silent'
-    elif 'hello' in lowered:
-        return 'Hello there!'
-    elif 'how are you' in lowered:
-        return 'Good, thanks!'
-    elif 'bye' in lowered:
-        return 'See you!'
-    elif 'roll dice' in lowered:
-        return f'You rolled: {randint(1, 6)}'
-    else:
-        return choice(['I\'m sorry, I don\'t understand.', 
-                       'What are you talking about?', 
-                       'Do you mind rephrasing that?'])
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo", messages=[{"role": "user", "content": user_input}]
+    )
+    return completion.choices[0].message.content
